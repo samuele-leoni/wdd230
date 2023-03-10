2bb2d1423a99f494972cbd9259eeeaad525a19de
samuele-leoni <leo22009@byui.edu>
2023-03-10 22:52:34 +0100

    Finished lesson10

---
+++ lesson10/scripts/weatherapi.js
@@ -0,0 +1,34 @@
+// select HTML elements in the document
+const currentTemp = document.getElementById('current-temp');
+const weatherIcon = document.getElementById('weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const apiKey = '719dfa891fd073b35614ca92bef10365'
+const url = `http://api.openweathermap.org/data/2.5/weather?id=524901&appid=${apiKey}`
+
+function displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+}
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data); // this is for testing the call
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
